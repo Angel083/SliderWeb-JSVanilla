619dc75400a19b7f26d58ac4f6fb123e53338327
Angel Gasca <cesargasca083@gmail.com>
2023-12-12 11:21:54 -0600

    feat: clickeable dots, autoplay

--- js/index.js
+++ js/index.js
@@ -2,26 +2,29 @@ class Slider{
   constructor(selector, options = {}){
     this.slider = document.querySelector(selector);
     this.options = {
-      interval: null,
       intervalMove: options.intervalMove || 2000,
+      autoPlay: options.hasOwnProperty('autoPlay') ? options.autoPlay : true,
       infinite: options.hasOwnProperty('infinite') ? options.infinite : true,
       controls: options.hasOwnProperty('controls') ? options.controls : true,
       controlsPosition: options.controlsPosition || 'inside' // inside or outside
-      // controls: options.hasOwnProperty('controls') ? options.controls : true,
     },
     this.move = this.move.bind(this);
+    this.interval = null,
+
     this.contador =  0;
     this.itemsCount = this.slider.querySelectorAll(".container > *").length;
-    this.start();
-    
     if (this.options.controls) {
       if (this.options.controlsPosition == "outside") {
         this.slider.classList.add("outside")
       }
       this.buildControls()
     }
+    this.start();
+    this.bindEvents();
+    
   }
   start(){
+    if (!this.options.autoPlay) return
     this.interval = window.setInterval(this.move, this.options.intervalMove);
   }
   move(){
@@ -39,11 +42,14 @@ class Slider{
     this.slider.querySelector(".controls li:nth-child("+ (index + 1) + ")").classList.add("active")
   }
   buildControls(){
+    var listDots = document.createElement("ul");
+    listDots.classList.add("dots")
+    this.slider.querySelector(".controls").appendChild(listDots);
     for (let i = 0; i < this.itemsCount; i++) {
       var control = document.createElement("li");
       control.classList.add("dot")
       if(i == 0) control.classList.add("active");
-      this.slider.querySelector(".controls ul").appendChild(control);
+      this.slider.querySelector(".controls .dots").appendChild(control);
     }
   }
   resetIndicator(){
@@ -51,6 +57,29 @@ class Slider{
       .forEach(element => {
         element.classList.remove("active");
       });
+  }
+
+  bindEvents(){
+    this.slider.addEventListener('click', (e) => {
+      if (e.target.matches(".dot")) {
+        let index = this.getIndex(e.target)
+        this.contador = index 
+        this.restartInterval();
+        this.moveTo(index);
+      }
+    })
+  }
+  getIndex(dot){
+    var children = dot.parentNode.children;
+    for (let i = 0; i < children.length; i++) {
+      if (children[i] == dot) return i 
+    }
+  }
+  restartInterval(){
+    if (this.interval) {
+       window.clearInterval(this.interval);
+       this.start();
+    }
   }
 }
 
