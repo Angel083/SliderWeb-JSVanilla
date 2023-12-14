4b6d5723bc54f37ba1b9f51b51ef8c4ecf9fdd45
Angel Gasca <cesargasca083@gmail.com>
2023-12-14 10:06:45 -0600

    feat: slider with arrows not infinite

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ class Slider{
     this.interval = null,
 
     this.contador =  0;
-    this.itemsCount = this.slider.querySelectorAll(".container > *").length;
+    this.sliderElementsCount = this.slider.querySelectorAll(".container > .slide__element").length;
     if (this.options.controls) {
       if (this.options.controlsPosition == "outside") {
         this.slider.classList.add("outside")
@@ -24,15 +24,17 @@ class Slider{
     
   }
   start(){
+    // if (!this.options.infinite && this.contador == 0)
+    //   this.slider.querySelector(".arrow_left").classList.add("disable")
     if (!this.options.autoPlay) return
     this.interval = window.setInterval(this.move, this.options.intervalMove);
   }
   move(){
     this.contador++;
     if (!this.options.infinite) {
-      if (this.contador > this.itemsCount - 1) return
+      if (this.contador > this.sliderElementsCount - 1) return
     }
-    if (this.contador > this.itemsCount - 1) this.contador = 0
+    if (this.contador > this.sliderElementsCount - 1) this.contador = 0
     this.moveTo(this.contador);
   }
   moveTo(index){
@@ -45,12 +47,30 @@ class Slider{
     var listDots = document.createElement("ul");
     listDots.classList.add("dots")
     this.slider.querySelector(".controls").appendChild(listDots);
-    for (let i = 0; i < this.itemsCount; i++) {
+    for (let i = 0; i < this.sliderElementsCount; i++) {
       var control = document.createElement("li");
       control.classList.add("dot")
       if(i == 0) control.classList.add("active");
       this.slider.querySelector(".controls .dots").appendChild(control);
     }
+
+    // var arrowContainer = document.createElement("div");
+    // arrowContainer.classList.add("arrow__container")
+    const middleContent = this.slider.children[0].clientHeight / 2 ;
+    var arrowLeft = document.createElement("span");
+    arrowLeft.classList.add("arrow_left");
+    arrowLeft.innerHTML = `<i class="fa-solid fa-chevron-left"></i>`;
+    arrowLeft.style.top = "calc("+ middleContent + "px )"
+
+    var arrowRight = document.createElement("span");
+    arrowRight.innerHTML = `<i class="fa-solid fa-chevron-right"></i>`;
+    arrowRight.classList.add("arrow_right");
+    arrowRight.style.top = "calc("+ middleContent + "px)"
+
+
+    this.slider.querySelector(".controls").appendChild(arrowLeft);
+    this.slider.querySelector(".controls").appendChild(arrowRight);
+
   }
   resetIndicator(){
     this.slider.querySelectorAll(".controls li.active")
@@ -66,6 +86,31 @@ class Slider{
         this.contador = index 
         this.restartInterval();
         this.moveTo(index);
+      }
+      if (e.target.matches(".arrow_left")) {
+        // if (!this.options.infinite && this.contador == 0) {
+        //   e.target.classList.add("disable")
+        //   console.log("loc")
+        //   return
+        // }
+        this.contador--;
+        this.restartInterval();
+        this.moveTo(this.contador);
+      }
+      if (e.target.matches(".arrow_right")) {
+        console.log("click")
+        console.log(this.contador)
+        console.log(this.sliderElementsCount- 1)
+        // console.log(this.sliderElementsCount)
+        if (this.contador == (this.sliderElementsCount - 1)) {
+          
+          console.log("moviendo primer child")
+          return
+        }
+        this.contador++;
+        this.restartInterval();
+        console.log("moviendo")
+        this.moveTo(this.contador);
       }
     })
   }
